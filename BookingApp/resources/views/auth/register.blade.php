879f5ffebda6e541096a4b53c92937b846c8dd79
Dawid Koszczuk <xdawidx58@gmail.com>
2023-05-15 23:55:04 +0200

    add welcome page (html,css) and some images

--- BookingApp/resources/views/auth/register.blade.php
+++ BookingApp/resources/views/auth/register.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    body {
+        background-color: #7fc142;
+    }
+</style>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
